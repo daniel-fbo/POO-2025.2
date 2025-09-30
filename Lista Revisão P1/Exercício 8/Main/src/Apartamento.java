07a97862816e4af196ebeddafa4a57b74d21aac1
Daniel Filipe <251019771@aluno.unb.br>
2025-09-30 12:39:33 -0300

    lista...

---
+++ Lista Revisão P1/Exercício 8/Main/src/Apartamento.java
@@ -0,0 +1,17 @@
+public class Apartamento {
+    private static float area;
+    private static int quartos;
+    private static int andar;
+    private static float valorDeCompra;
+    private static int vagasDeGaragem;
+    private static boolean temVaranda;
+
+    public void exibirInfo(){
+        System.out.println(area);
+        System.out.println(quartos);
+        System.out.println(andar);
+        System.out.println(valorDeCompra);
+        System.out.println(vagasDeGaragem);
+        System.out.println(temVaranda);
+    }
+}
